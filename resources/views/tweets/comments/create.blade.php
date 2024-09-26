392e60057714e66108a5d15b3c0a4a7e689bdc0c
KOU050223 <kouhei20050223@icloud.com>
2024-09-26 19:34:30 +0900

    [modify]コメントCRUD処理追加

--- resources/views/tweets/comments/create.blade.php
+++ resources/views/tweets/comments/create.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
   <x-slot name="header">
     <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-      {{ __('コメント詳細') }}
+      {{ __('コメント作成') }}
     </h2>
   </x-slot>
 
@@ -10,23 +10,17 @@
       <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
         <div class="p-6 text-gray-900 dark:text-gray-100">
           <a href="{{ route('tweets.show', $tweet) }}" class="text-blue-500 hover:text-blue-700 mr-2">Tweetに戻る</a>
-          <p class="text-gray-600 dark:text-gray-400 text-sm">{{ $tweet->tweet }}: {{ $tweet->user->name }}</p>
-          <p class="text-gray-800 dark:text-gray-300 text-lg">{{ $comment->comment }}</p>
-          <p class="text-gray-600 dark:text-gray-400 text-sm">{{ $comment->user->name }}</p>
-          <div class="text-gray-6000 dark:text-gray-400 text-sm">
-            <p>コメント作成日時: {{ $comment->created_at->format('Y-m-d H:i') }}</p>
-            <p>コメント更新日時: {{ $comment->updated_at->format('Y-m-d H:i') }}</p>
-          </div>
-          @if (auth()->id() === $comment->user_id)
-          <div class="flex mt-4">
-            <a href="{{ route('tweets.comments.edit', [$tweet, $comment]) }}" class="text-blue-500 hover:text-blue-700 mr-2">編集</a>
-            <form action="{{ route('tweets.comments.destroy', [$tweet, $comment]) }}" method="POST" onsubmit="return confirm('本当に削除しますか？');">
-              @csrf
-              @method('DELETE')
-              <button type="submit" class="text-red-500 hover:text-red-700">削除</button>
-            </form>
-          </div>
-          @endif
+          <form method="POST" action="{{ route('tweets.comments.store', $tweet) }}">
+            @csrf
+            <div class="mb-4">
+              <label for="comment" class="block text-gray-700 dark:text-gray-300 text-sm font-bold mb-2">コメント</label>
+              <input type="text" name="comment" id="comment" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 dark:text-gray-300 dark:bg-gray-700 leading-tight focus:outline-none focus:shadow-outline">
+              @error('comment')
+              <span class="text-red-500 text-xs italic">{{ $message }}</span>
+              @enderror
+            </div>
+            <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">コメントする</button>
+          </form>
         </div>
       </div>
     </div>
