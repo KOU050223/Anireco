bae61bc8b6381436c06332e55040145ec6e7dbf0
KOU050223 <kouhei20050223@icloud.com>
2024-09-26 15:42:41 +0900

    [add]like/dislike機能追加

--- app/Models/User.php
+++ app/Models/User.php
@@ -47,5 +47,10 @@ class User extends Authenticatable
     public function tweets()
     {
       return $this->hasMany(Tweet::class);
-    }    
+    }
+    
+    public function likes()
+    {
+        return $this->belongsToMany(Tweet::class)->withTimestamps();
+    }
 }
